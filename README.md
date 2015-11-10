34ef5465168ba2b426aabb3bab5ff998ac698a9b
Casey Foster <c@sey.me>
2015-11-10 13:48:29 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# cors-proxy
+
+A simple HTTP(S) CORS proxy.
+
+# Usage
+
+Run the server.
+
+```bash
+PORT=12345 node .
+```
+
+Make a request. The path is the URL of the proxy target.
+
+```bash
+curl localhost:12345/https://www.google.com
+```
---
+++ index.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const httpProxy = require('http-proxy');
+const url = require('url');
+
+const proxy = httpProxy.createProxyServer({
+  changeOrigin: true,
+  appendPath: false
+});
+
+const server = http.createServer((req, res) => {
+  console.log(`${req.method} ${req.url}`);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  const reqHeaders = req.headers['access-control-request-headers'];
+  if (reqHeaders) res.setHeader('Access-Control-Allow-Headers', reqHeaders);
+  if (req.method === 'OPTIONS') return res.end();
+
+  const parsed = url.parse(req.url.slice(1));
+  if (!parsed.protocol || !parsed.host) return res.end();
+
+  const target = `${parsed.protocol}//${parsed.host}`;
+  req.url = parsed.path;
+  proxy.web(req, res, {target});
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log(`Listening on port ${port}`));
